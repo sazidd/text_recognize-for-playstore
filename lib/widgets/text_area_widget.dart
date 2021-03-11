702ae12b86756215e11eff9b044a7eaaac41a728
sazidd <rehmansazid32@gmail.com>
2021-03-11 23:29:54 +0600

    adding icon-splash

--- lib/widgets/text_area_widget.dart
+++ lib/widgets/text_area_widget.dart
@@ -2,53 +2,69 @@ import 'package:flutter/material.dart';
 
 class TextAreaWidget extends StatelessWidget {
   final String text;
-  final VoidCallback onClickedCopy;
+  final VoidCallback onCopy;
   final VoidCallback onSpeak;
+  final double height;
 
   const TextAreaWidget({
     @required this.text,
-    @required this.onClickedCopy,
+    @required this.onCopy,
     @required this.onSpeak,
-    Key key,
-  }) : super(key: key);
+    @required this.height,
+  });
 
   @override
-  Widget build(BuildContext context) => Column(
-        children: [
-          Row(
-            children: [
-              Expanded(
-                child: Card(
-                  child: Container(
-                    height: 280,
-                    padding: EdgeInsets.all(8),
-                    alignment: Alignment.center,
-                    child: SelectableText(
-                      text.isEmpty ? 'Scan an Image to get text' : text,
-                      textAlign: TextAlign.center,
-                    ),
-                  ),
-                ),
-              ),
-              Column(
-                children: [
-                  IconButton(
-                    icon: Icon(
-                      Icons.copy,
-                      color: Colors.black,
-                      size: 15,
-                    ),
-                    color: Colors.grey[200],
-                    onPressed: onClickedCopy,
-                  ),
-                  IconButton(
-                    icon: Icon(Icons.volume_up),
-                    onPressed: () => onSpeak(),
-                  ),
-                ],
-              ),
-            ],
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        Card(
+          child: Container(
+            height: height,
+            padding: const EdgeInsets.symmetric(horizontal: 8),
+            alignment: Alignment.center,
+            child: SelectableText(
+              text.isEmpty ? 'Scan an Image to get text' : text,
+              textAlign: TextAlign.center,
+            ),
           ),
-        ],
-      );
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            IconButton(
+              icon: Icon(Icons.volume_up),
+              onPressed: () => onSpeak(),
+            ),
+            IconButton(
+              icon: Icon(
+                Icons.copy,
+                color: Colors.black,
+                size: 15,
+              ),
+              color: Colors.grey[200],
+              onPressed: onCopy,
+            ),
+          ],
+        ),
+      ],
+    );
+  }
 }
+
+// Column(
+//   children: [
+//     IconButton(
+//       icon: Icon(
+//         Icons.copy,
+//         color: Colors.black,
+//         size: 15,
+//       ),
+//       color: Colors.grey[200],
+//       onPressed: onClickedCopy,
+//     ),
+//     IconButton(
+//       icon: Icon(Icons.volume_up),
+//       onPressed: () => onSpeak(),
+//     ),
+//   ],
+// ),
